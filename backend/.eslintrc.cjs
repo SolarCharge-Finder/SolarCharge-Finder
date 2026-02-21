d5b2ab31350e198a4864d64d3e089be0b18a5fa7
Zero3Nine <67416696+zero3nine@users.noreply.github.com>
2026-02-21 20:19:39 +0530

    vitest global fix (the npm package isnt installed gg </3 )

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,12 +1,11 @@
 module.exports = {
-  plugins: ['vitest-globals'],
   env: {
     browser: true,
     node: true,
     es2021: true,
     jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:vitest-globals/recommended'],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
